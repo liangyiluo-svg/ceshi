e20a838bb7ebd22ae0df8371a5ab97d95bd8d0a5
1 <1@>
2020-04-14 08:55:12 +0800

    添加项目文件。

---
+++ ceshi/Controllers/DomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ceshi.Controllers
+{
+    public class DomeController : Controller
+    {
+        // GET: Dome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult caidan()
+        {
+            return View();
+        }
+    }
+}
